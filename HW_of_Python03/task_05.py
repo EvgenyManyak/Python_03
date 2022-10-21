ef49694d33ccbfec8b9e8e138bff854bb305be0e
Evgeny_Manyak <buriy-anarchist@inbox.ru>
2022-10-21 22:50:28 +0300

    add task 05

---
+++ HW_of_Python03/task_05.py
@@ -0,0 +1,19 @@
+# Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+
+n = int(input('Введите число: '))
+
+def get_fibonacci(n):
+    fibo_nums = []
+    a, b = 1, 1
+    for i in range(n-1):
+        fibo_nums.append(a)
+        a, b = b, a + b
+    a, b = 0, 1
+    for i in range (n):
+        fibo_nums.insert(0, a)
+        a, b = b, a - b
+    return fibo_nums
+
+fibo_nums = get_fibonacci(n)
+print(get_fibonacci(n))
+print(fibo_nums.index(0))
